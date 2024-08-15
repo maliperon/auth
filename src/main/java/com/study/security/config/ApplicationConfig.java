335ed35bb877cc56d60acd2de4d249e0e4bbd74c
Eduarda Martins <eduarda07martins@gmail.com>
2024-08-15 12:12:04 -0300

    Create ApplicationConfig class

---
+++ src/main/java/com/study/security/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.study.security.config;
+
+import com.study.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+
+}
