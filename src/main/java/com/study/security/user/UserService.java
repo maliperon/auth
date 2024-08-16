70f19891e4fb7764d91a4f6bcf0ae69fbd4a87ab
Eduarda Martins <eduarda07martins@gmail.com>
2024-08-16 09:41:57 -0300

    Create UserService class

---
+++ src/main/java/com/study/security/user/UserService.java
@@ -0,0 +1,21 @@
+package com.study.security.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
