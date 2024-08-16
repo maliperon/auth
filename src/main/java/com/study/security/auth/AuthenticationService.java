c61387aeda515beba09f6cfbd4e06be90d30d40b
Eduarda Martins <eduarda07martins@gmail.com>
2024-08-16 12:20:05 -0300

    Add token confirmation to AuthenticationService

--- src/main/java/com/study/security/auth/AuthenticationService.java
+++ src/main/java/com/study/security/auth/AuthenticationService.java
@@ -1,37 +1,97 @@
 package com.study.security.auth;
 
+import com.study.security.auth.token.ConfirmationToken;
+import com.study.security.auth.token.ConfirmationTokenService;
 import com.study.security.config.JwtService;
 import com.study.security.user.Role;
 import com.study.security.user.User;
-import com.study.security.user.UserRepository;
+import com.study.security.user.UserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final EmailValidator emailValidator;
+    private final UserService userService;
+    private final ConfirmationTokenService confirmationTokenService;
 
     public AuthenticationResponse register(RegisterRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        Optional<User> userExists = userService.findUserByEmail(request.getEmail());
+
+        if(!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+
+        if(userExists.isPresent()) {
+            throw new IllegalStateException("email already taken");
+        }
+
         var user = User.builder()
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
+                .enabled(false)
+                .locked(false)
                 .build();
-        repository.save(user);
+
+        userService.saveUser(user);
+
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                user
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+//        TODO: send email
+
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    @Transactional
+    public String confirmToken(String token) {
+        Optional<ConfirmationToken> confirmationToken = confirmationTokenService.getConfirmationToken(token);
+
+        if(confirmationToken.isEmpty()) {
+            throw new IllegalStateException("token not found");
+        }
+
+        if(confirmationToken.get().getConfirmedAt() != null) {
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.get().getExpiredAt();
+
+        if(expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        userService.enableUser(confirmationToken.get().getUser().getEmail());
+
+        return "";
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
@@ -41,7 +101,7 @@ public class AuthenticationService {
                         request.getPassword()
                 )
         );
-        var user = repository.findByEmail(request.getEmail())
+        var user = userService.findUserByEmail(request.getEmail())
                 .orElseThrow();
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
