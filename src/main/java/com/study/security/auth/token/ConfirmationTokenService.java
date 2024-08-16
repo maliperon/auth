a406bb8b2f09d63cb4b63b45cfd96616c17cd5ac
Eduarda Martins <eduarda07martins@gmail.com>
2024-08-16 12:18:23 -0300

    Add save and get methods in ConfirmationTokenService

--- src/main/java/com/study/security/auth/token/ConfirmationTokenService.java
+++ src/main/java/com/study/security/auth/token/ConfirmationTokenService.java
@@ -3,6 +3,9 @@ package com.study.security.auth.token;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class ConfirmationTokenService {
@@ -11,5 +14,20 @@ public class ConfirmationTokenService {
 
     public void saveConfirmationToken(ConfirmationToken token){
         confirmationTokenRepository.save(token);
+    }
+
+    public void setConfirmedAt(String token){
+        Optional<ConfirmationToken> confirmationToken = getConfirmationToken(token);
+
+        if(confirmationToken.isPresent()) {
+            confirmationToken.ifPresent(t -> {
+                t.setConfirmedAt(LocalDateTime.now());
+                confirmationTokenRepository.save(t);
+            });
+        }
+    }
+
+    public Optional<ConfirmationToken> getConfirmationToken(String token){
+        return confirmationTokenRepository.findByToken(token);
     }
 }
